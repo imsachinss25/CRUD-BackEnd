7b454e09a73e2bcc7ae2544b702b7088724d0ba1
imsachinss25 <63443790+imsachinss25@users.noreply.github.com>
2021-04-21 22:33:40 +0530

    Update StudentController.js

--- src/controllers/StudentController.js
+++ src/controllers/StudentController.js
@@ -1,51 +1,11 @@
 const router = require("express").Router();
-router.post("/students",(req,res,next)=>{
+const express = require('express')
 
-   console.log(req.body);
-   const user = new Student(req.body);
-   user.save().then(()=>{
-       res.status(201).send(user);
-   }).catch((e)=>{
-       console.log(e);
-       res.status(400).send(e);
-  
-   })
+const { createStudent, getStudent, deleteStudent, updateEmployee } = require('../dao/students');
 
-});
+router.post("/students", createStudent);
+router.get("/students", getStudent)
+router.patch("/students/:id", updateStudent)
+router.delete("/students/:id", deleteStudent)
 
-router.get("/students", async (req, res,next)=>{
-    try{
-        const studentData = await Student.find();
-        res.send(studentData);
-    }
-    catch(e){
-        res.send(e);
-    }
-})
-
-router.patch("/students/:id", async (req, res)=>{
-    try{
-        const _id = req.params.id;
-        const updateStudents = await Student.findByIdAndUpdate(_id, req.body,{
-            new : true
-        });
-        res.send(updateStudents);
-    }catch(e){
-        res.status(400).send(e);
-    }
-})
-
-router.delete("/students/:id", async (req, res)=>{
-    try{
-        const _id = req.params.id;
-        const deleteStudents = await Student.findByIdAndDelete(_id);
-        if(!req.params.id)
-        {
-            return res.status(400).send();
-        }
-        res.send(deleteStudents);
-    }catch(e){
-        res.status(500).send(e);
-    }
-})
 module.exports=router;
